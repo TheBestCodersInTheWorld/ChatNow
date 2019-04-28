ba0e02c12ff48f4559b70573cc5c312e73f28223
Amy DaliaSandberg David <DaliaSandberg@Dalias-MacBook-Air.local>
2019-04-28 17:41:04 -0500

    Added a menu

--- src/client.py
+++ src/client.py
@@ -12,6 +12,32 @@ class User(object):
 		username = raw_input("Please input your name: ").strip()
 		app.register_user(username)
 
-		cmd = raw_input("please input a command: ").strip()
-		if cmd == "show users":
-			print(app.show_users())
+		while (True):
+			self.menu()
+			# this takes the input from the user and makes cmd that input result
+			cmd = raw_input("please input a command: ").strip()
+
+			# if the user specified show users as an option
+			if cmd == "show users" or cmd == "s":
+				print(app.show_users())
+
+
+
+	def menu(self):
+		print("here are some choices")
+		print("s | show users")
+
+	def make_hotdog(self, hotdog_type):
+		return None
+
+
+# def make_hotdog(hotdog_type):
+# 	return None
+
+
+# make_hotdog("the hotdog is whatever")
+
+user1 = User()
+user2 = User()
+user1.make_hotdog("the hotdog is spicy")
+user2.make_hotdog("the hotdog is not spicy")
